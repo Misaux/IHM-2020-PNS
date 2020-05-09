3c56152b3076c8a34fec371a08742efb93665794
Misaux <jessy.dussart@gmail.com>
2020-05-09 17:50:30 +0200

    fix

---
+++ TD3-Bateau/app/src/main/java/com/TD3/bateau/activities/TimelineActivity.java
@@ -0,0 +1,15 @@
+package com.TD3.bateau.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.TD3.bateau.R;
+
+public class TimelineActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
